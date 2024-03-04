5c7d18412bc3fbd502ec7a58de799d635d743ece
Abdul <aabdulmajeed.isa@gmail.com>
2024-03-04 14:01:13 +0100

    complete pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+"""
+Pascal triangle module
+"""
+
+
+def pascal_triangle(n):
+    """
+    Pascal triangle
+    """
+    if n <= 0:
+        return []
+    triangle = []
+    c = 1
+    while (c <= n):
+        row = [1] * c
+        if c <= 2:
+            triangle.append(row)
+            c += 1
+        else:
+            for i in range(len(triangle[-1]) - 1):
+                row[1 + i] = triangle[-1][i] + triangle[-1][i + 1]
+            triangle.append(row)
+            c += 1
+
+    return triangle
