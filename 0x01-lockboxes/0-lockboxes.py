0bbc0c6f5c2758977b53f7e51dbcd63d48dfe3b6
Abdul <aabdulmajeed.isa@gmail.com>
2024-03-12 12:27:09 +0100

    done 0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Lockboxes module
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Lockboxes
+    """
+    keys = boxes[0]
+    d_boxes = {}
+    unlocked_boxed = {0}
+
+    for i in range(len(boxes)):
+        d_boxes[i] = boxes[i]
+
+    for key in keys:
+        if key not in unlocked_boxed:
+            keys += d_boxes[key]
+            d_boxes[key] = []
+            unlocked_boxed.add(key)
+    return len(unlocked_boxed) == len(boxes)
