5512b0be013cf7540affaabbf73e6f7279e8de87
Abdul <aabdulmajeed.isa@gmail.com>
2024-03-12 13:16:35 +0100

    fix 0-lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,12 +10,9 @@ def canUnlockAll(boxes):
     """
     if not isinstance(boxes, list) or not boxes or not boxes[0]:
         return False
-    keys = boxes[0][:]
-    d_boxes = {}
+    keys = boxes[0]
+    d_boxes = {i: box for i, box in enumerate(boxes)}
     unlocked_boxed = {0}
-
-    for i in range(len(boxes)):
-        d_boxes[i] = boxes[i]
 
     for key in keys:
         if len(unlocked_boxed) < len(boxes) and key not in unlocked_boxed:
