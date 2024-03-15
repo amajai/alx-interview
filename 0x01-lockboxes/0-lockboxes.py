b1e2bb505af88d3fd10b623492dc1f476e05b0b8
Abdul <aabdulmajeed.isa@gmail.com>
2024-03-15 13:41:49 +0100

    correct 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -18,5 +18,5 @@ def canUnlockAll(boxes):
         if len(unlocked_boxed) < len(boxes) and key not in unlocked_boxed:
             if (d_boxes.get(key) is not None):
                 keys += d_boxes[key]
-            unlocked_boxed.add(key)
+                unlocked_boxed.add(key)
     return len(unlocked_boxed) == len(boxes)
